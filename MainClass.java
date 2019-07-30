8cb84789a13eb957a742911364f2e75dc491c9ff
Chirag <dawra1997@gmail.com>
2019-07-30 14:48:30 +0530

    hello jenkins

---
+++ MainClass.java
@@ -0,0 +1,13 @@
+package hellojavajenkins;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello1 ");
+		System.out.println("hello2 ");
+		System.out.println("hello3 ");
+	}
+
+}
