c07aeed4b5f7df7b7f0e1859b110a949cc71b4a1
Chirag <dawra1997@gmail.com>
2019-07-30 14:52:18 +0530

    Second commit

--- MainClass.java
+++ MainClass.java
@@ -5,9 +5,14 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("hello1 ");
-		System.out.println("hello2 ");
-		System.out.println("hello3 ");
+		//System.out.println("hello1 ");
+		//System.out.println("hello2 ");
+		//System.out.println("hello3 ");
+	   int i;
+	   for(i=0;i<5;i++)
+	   {
+		   System.out.println("hello ");
+	   }
 	}
 
 }
